757e64f7615ab7ae90eeec315f27fd2ff0ba1dc8
Aditya Kharadkar <adityakharadkar27@gmail.com>
2024-02-14 10:52:48 +0530

    Add entered goal and summary using form

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,12 +1,19 @@
 import { useRef, type FormEvent } from "react";
 
-export default function NewGoal() {
-  const goal = useRef(null);
-  const summary = useRef(null);
+type NewGoalProps = {
+  onAddGoal: (goal: string, summary: string) => void;
+};
+
+export default function NewGoal({ onAddGoal }: NewGoalProps) {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     // new FormData(event.currentTarget);
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
+    onAddGoal(enteredGoal, enteredSummary);
   }
 
   return (
