574807f70aee1cd402fb112ed511bf5f6d9f4a3f
Aditya Kharadkar <adityakharadkar27@gmail.com>
2024-02-17 17:16:50 +0530

    Add more complex component which uses a union of two type props

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,8 @@
+import { type ReactNode } from "react";
+
 import CourseGoal from "./CourseGoal";
 import { CourseGoal as CGoal } from "../App";
+import InfoBox from "./InfoBox";
 
 type CourseGoalListProps = {
   goals: CGoal[];
@@ -10,15 +13,36 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="medium">
+        You are collecting a lot of goals. Don't put too much n your plate.
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
